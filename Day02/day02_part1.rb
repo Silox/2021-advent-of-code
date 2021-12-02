c5fd3107c9aa41ef4aa4f5cd9cd9373a2820f8d1
Tom Naessens <tom.naessens@mediagenix.tv>
2021-12-02 18:00:10 +0100

    Day02

---
+++ Day02/day02_part1.rb
@@ -0,0 +1,24 @@
+file = File.open("input")
+
+Instruction = Struct.new(:command, :amount)
+
+instructions = file.readlines.map(&:chomp).map do |line|
+  command, amount = line.split()
+  Instruction.new(command, amount.to_i)
+end
+
+horizontal = 0
+depth = 0
+
+instructions.each { |instruction|
+  case instruction.command
+  when 'forward'
+    horizontal += instruction.amount
+  when 'down'
+    depth += instruction.amount
+  when 'up'
+    depth -= instruction.amount
+  end
+}
+
+puts horizontal * depth
---
+++ Day02/day02_part2.rb
@@ -0,0 +1,28 @@
+file = File.open("input")
+
+Instruction = Struct.new(:command, :amount)
+
+instructions = file.readlines.map(&:chomp).map do |line|
+  command, amount = line.split
+  Instruction.new(command, amount.to_i)
+end
+
+horizontal = 0
+aim = 0
+depth = 0
+
+instructions.each { |instruction|
+  case instruction.command
+  when 'forward'
+    horizontal += instruction.amount
+    depth += instruction.amount * aim
+  when 'down'
+    aim += instruction.amount
+  when 'up'
+    aim -= instruction.amount
+  else
+    raise NotImplementedError.new
+  end
+}
+
+puts horizontal * depth

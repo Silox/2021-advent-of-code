3a855d7a108790750383cd1f0151614433b33af4
Tom Naessens <tom.naessens@mediagenix.tv>
2021-12-06 20:09:26 +0100

    Alternative solution

--- Day06/day06_part2.rb
+++ Day06/day06_part2.rb
@@ -2,26 +2,16 @@ file = File.open("input")
 
 fishes = file.readline.split(',').map(&:to_i)
 
-@cache = {}
-
-def breed(counter, days_left)
-  return 1 if days_left.zero?
-  return 1 if counter >= days_left
-
-  remaining_days = days_left - 1
-
-  cached_value = @cache[days_left][counter] unless @cache[days_left].nil?
-  return cached_value unless cached_value.nil?
-
-  result = if counter.zero?
-             breed(6, remaining_days) + breed(8, remaining_days)
-           else
-             breed(counter - 1, remaining_days)
-           end
-
-  @cache[days_left] = {} if @cache[days_left].nil?
-  @cache[days_left][counter] = result
-  result
+tally = [0] * 9
+fishes.tally.each do |k, v|
+  tally[k] = v
 end
 
-pp fishes.tally.map { |fish, tally| tally * breed(fish, 256) }.sum
+256.times do
+  breeders = tally.shift
+
+  tally[6] += breeders
+  tally[8] = breeders
+end
+
+pp tally.sum
